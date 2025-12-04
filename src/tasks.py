5e0cf3df436ee73465f27d3283fb68797e89994a
bona2020 <bona@infonix.net>
2025-12-05 01:12:10 +0530

    tasks returning the requests

--- src/tasks.py
+++ src/tasks.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Request,Form
+from fastapi.templating import Jinja2Templates
 from utils import db
 
+templates=Jinja2Templates(directory='templates')
 
 router = APIRouter(tags=['Tasks'])
 @router.get('/taks')
 def root_taks():
-    return {"Message":"Hello from Tasks Root"}
+    res =db.table('tasks').select('*').execute()
+    data = res.data
+    return templates.TemplateResponse('home.html',{'request':Request,'tasks':data})
