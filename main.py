1771200b9a66e3d79e705a317055631ab9050c52
shiva bodduna <shivabodduna123@gmail.com>
2025-12-06 14:09:23 +0530

    Add mangum handler for Vercel deployment

--- main.py
+++ main.py
@@ -4,16 +4,13 @@ from src.projects import router as projects_router
 from src.tasks import router as tasks_router
 
 app = FastAPI()
+
 @app.get('/')
 def read_root():
     return {"Message":"Hello from Root"}
 
 app.include_router(projects_router)
 app.include_router(tasks_router)
-#     return {"Message":"Hello from Root"} 
 
-
-# app.include_router(projects_router, prefix="/projects")
-# app.include_router(tasks_router, prefix="/tasks")
-
-handler = Mangum(app)
+# Change this line - wrap it in a function
+handler = Mangum(app, lifespan="off")
