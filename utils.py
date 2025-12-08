2c9a073322e06fdde5ee994e63820d7920de43b8
shiva bodduna <shivabodduna123@gmail.com>
2025-12-08 16:18:59 +0530

    committed

--- utils.py
+++ utils.py
@@ -7,3 +7,15 @@ db_link = os.getenv("database_url")
 api_key = os.getenv('api')
 
 db = create_client(db_link,api_key)
+
+
+# import os
+# from supabase import create_client
+
+# db_link = os.getenv("SUPABASE_URL")
+# api_key = os.getenv("SUPABASE_KEY")
+
+# if not db_link or not api_key:
+#     raise Exception("Supabase environment variables missing!")
+
+# db = create_client(db_link, api_key)
